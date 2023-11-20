f5be4456c3ce88e577038993378150a9e25662c2
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-11-20 19:45:28 +0530

    updated

---
+++ SQL/Clauses/Limit Clause/limit.sql
@@ -0,0 +1,4 @@
+USE college;
+
+SELECT * FROM student1 
+WHERE marks<24 LIMIT 3;
---
+++ SQL/Clauses/Order_by_clause/orderby.sql
@@ -0,0 +1,14 @@
+USE college;
+
+SELECT * FROM student1 
+ORDER BY marks  ASC;
+
+USE college;
+
+SELECT * FROM student1 
+ORDER BY marks  DESC;
+
+USE college;
+
+SELECT * FROM student1 
+ORDER BY city  DESC;
