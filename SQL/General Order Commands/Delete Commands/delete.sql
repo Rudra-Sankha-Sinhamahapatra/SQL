35d757235db5b0bcab79fd02c97fdc96db73af79
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-11-22 17:40:09 +0530

    updated

---
+++ SQL/General Order Commands/Delete Commands/delete.sql
@@ -0,0 +1,13 @@
+USE college;
+
+SELECT * FROM student1;
+
+USE college;
+
+INSERT INTO student1 
+VALUES(6,"Erling",20,'B',"Norway");
+
+USE college;
+
+DELETE FROM student1
+WHERE marks=20;
--- SQL/General Order Commands/Update Commands/update.sql
+++ SQL/General Order Commands/Update Commands/update.sql
@@ -13,3 +13,18 @@ USE college;
 UPDATE student1
 SET city="Delhi" 
 WHERE city="Pune" AND marks=25 ;
+
+-- SET SQL_SAFE_UPDATES=0;
+--  turns off the safe mode
+-- of sql and database can be updated
+
+-- SET SQL_SAFE_UPDATES=1;
+-- sets the sql to be safe and database
+-- can't be updated
+
+USE college;
+
+UPDATE student1 
+SET marks=25,grade='A' 
+WHERE rollno=5;
+
