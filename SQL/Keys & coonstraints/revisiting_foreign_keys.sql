b40ba635123614ed30e2e2b4634ad0d1074f00fc
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-11-25 12:28:39 +0530

    updated

--- SQL/Keys & coonstraints/revisiting_foreign_keys.sql
+++ SQL/Keys & coonstraints/revisiting_foreign_keys.sql
@@ -10,10 +10,47 @@ id INT PRIMARY KEY,
 name VARCHAR(50)
 );
 
+USE college;
+
+INSERT INTO dept VALUES
+(101,"english"),
+(102,"IT")
+
+USE college;
+
+UPDATE dept
+SET id=103
+WHERE id=102;
+
+USE college;
+
+UPDATE dept 
+SET id=111
+WHERE id=101;
+
+USE college;
+SELECT * FROM dept;
+
+USE college;
+
 CREATE TABLE IF NOT EXISTS teacher(
 id INT PRIMARY KEY,
 name VARCHAR(50),
 dept_id INT,
 FOREIGN KEY (dept_id) REFERENCES dept(id)
+ON UPDATE CASCADE
+ON DELETE CASCADE
 );
 
+USE college;
+DROP TABLE teacher;
+
+USE college;
+
+INSERT INTO teacher VALUES
+(101,"Aman",101),
+(102,"Alakh",102)
+
+USE college;
+
+SELECT * FROM teacher;
